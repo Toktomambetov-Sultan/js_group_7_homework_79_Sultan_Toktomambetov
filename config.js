b91ace56f0a0c97af5984406fc0b7097d4a01998
sultan <sultan.toktomambetov.04@bk.ru>
2020-10-24 03:24:49 +0600

    Added logic of file Uploader.

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
 const rootDir = __dirname;
+
+
 module.exports = {
     port: 8000,
     rootDir,
+    imageFolder: path.join(rootDir, "/public/images"),
     db: {
         host: "localhost",
         user: "root",
--- routers/resourcesRouter.js
+++ routers/resourcesRouter.js
@@ -1,39 +1,61 @@
 const express = require("express");
+const multer = require("multer");
+const { nanoid } = require("nanoid");
+const config = require("../config");
 const MysqlTool = require("../mysql");
+const fs = require("fs").promises;
 const router = express.Router();
 
-const routerFunction = (db, resource) => {
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, config.imageFolder);
+    },
+    filename: function (req, file, cb) {
+        cb(null, nanoid() + file.originalname);
+    }
+});
 
+const upload = multer({ storage });
+
+const routerFunction = (db, resource) => {
     const mysqlTool = new MysqlTool(db);
 
     router.get("/" + resource, async (req, res) => {
         try {
             res.send(await mysqlTool.getItems(resource));
         } catch (error) {
-            res.send(error);
+            res.status(400).send(error);
         }
     });
     router.get("/" + resource + "/:id", async (req, res) => {
+        const data = (await mysqlTool.getItemsById(resource, req.params.id))[0]
+            || { error: "not found" };
         try {
-            res.send((await mysqlTool.getItemsById(resource, req.params.id))[0] || { error: "not found" });
+            res.send();
         } catch (error) {
-            res.send(error);
+            res.status(400).send(error);
         }
     });
 
-    router.post("/" + resource, async (req, res) => {
+    router.post("/" + resource, upload.single("image"), async (req, res) => {
+        const data = { ...req.body, image: req.file ? req.file.filename : null };
         try {
-            res.send(await mysqlTool.post(resource, req.body));
+            res.send(await mysqlTool.post(resource, data));
         } catch (error) {
-            res.send(error);
+            await fs.unlink(config.imageFolder + "/" + data.image);
+            res.status(400).send(error);
+
         }
     });
 
     router.delete("/" + resource + "/:id", async (req, res) => {
+        const data = (await mysqlTool.getItemsById(resource, req.params.id))[0];
         try {
+            console.log(data, data.image);
+            data.image && await fs.unlink(config.imageFolder + "/" + data.image, () => { });
             res.send(await mysqlTool.delete(resource, req.params.id));
         } catch (error) {
-            res.send(error);
+            res.status(400).send(error);
         }
     });
 
